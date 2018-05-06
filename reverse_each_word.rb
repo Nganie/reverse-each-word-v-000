a89d0d87dbb8279a5a86e2c95d2a063a80c695c2
Nganie <ngan.tp.quan@gmail.com>
2018-05-06 13:52:11 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
-def reverse_each_word(str)
-  x = str.split.collect do |word| word.reverse
+def reverse_each_word(sentence)
+  x = sentence.split.collect do |word| word.reverse
 end
-x.join("")
+x.join(" ")
 end
